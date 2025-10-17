0f16b5fa06181860938540bd8fdfbc36b2d22124
Artur Tavares <arturtmelo2@gmail.com>
2025-10-17 06:40:28 -0300

    Melhoriasdsssdasfaasdas

--- src/App.tsx
+++ src/App.tsx
@@ -198,7 +198,7 @@ function App() {
           }
         }}
         className={`fixed bottom-6 right-6 p-3 ${
-          showForm ? 'bg-red-600 hover:bg-red-700' : 'bg-gray-900 hover:bg-red-600'
+          showForm ? 'bg-red-600 hover:bg-red-700' : 'bg-gray-900 hover:bg-green-700'
         } text-white rounded-full shadow-lg transition-all transform hover:scale-110 z-[1000]`}
         title={showForm ? 'Fechar formulário' : 'Nova transação'}
       >
