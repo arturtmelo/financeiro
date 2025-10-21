c21ac4a3335eb67b87c19d17d7c4299f83f14208
Artur Tavares <arturtmelo2@gmail.com>
2025-10-21 20:54:22 -0300

    feat: adicionar botão Nova Transação na aba Análises com redirecionamento

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,6 @@ const Header = () => {
             title="Sair"
           >
             <LogOut className="w-4 h-4" />
-            <span className="hidden sm:inline">Sair</span>
           </button>
         </div>
       </div>
